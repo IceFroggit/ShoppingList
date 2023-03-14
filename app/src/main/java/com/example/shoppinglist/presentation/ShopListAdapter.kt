c175d6640f5ced2d4c17ef241a9571137b8e61b8
IceFroggit <zeksengali0@gmail.com>
2023-03-14 11:47:52 +0300

    made ShopListDiffCallback for redraw only one deleted itemView (better optimisation)

--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -2,26 +2,29 @@ package com.example.shoppinglist.presentation
 
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+    var count = 0
     var shopList = listOf<ShopItem>()
         set(value) {
+            //чтобы не использовать onDatafyDataChanged
+            val callback = ShopListDiffCallback(shopList, value)
+            val diffResult = DiffUtil.calculateDiff(callback)
+            diffResult.dispatchUpdatesTo(this)
             field = value
-            notifyDataSetChanged()
         }
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
     var onShopItemClickListener: ((ShopItem) -> Unit)? = null
 
     //метод как создать вью
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-
         val layout = when (viewType) {
             VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
             VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
@@ -34,15 +37,16 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     // метод как вставить значения в это вью
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
         val shopItem = shopList[position]
+        Log.d("ShopListAdapter", "OnBindViewHolder, count: ${++count}")
         holder.view.setOnLongClickListener {
             //вызов лямбды выражения
             onShopItemLongClickListener?.invoke(shopItem)
             true
         }
-        holder.view.setOnClickListener{
+        holder.view.setOnClickListener {
             onShopItemClickListener?.invoke(shopItem)
         }
-        holder.tvName.text = "${shopItem.name}"
+        holder.tvName.text = shopItem.name
         holder.tvCount.text = shopItem.count.toString()
 
     }
@@ -63,13 +67,11 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
         val tvName = view.findViewById<TextView>(R.id.tv_name)
         val tvCount = view.findViewById<TextView>(R.id.tv_count)
     }
-    interface OnShopItemLongClickListener{
-        fun onShopItemLongClick(shopItem: ShopItem)
-    }
 
     companion object {
-        const val  VIEW_TYPE_ENABLED = 100
-        const val  VIEW_TYPE_DISABLED = 110
+        const val VIEW_TYPE_ENABLED = 100
+        const val VIEW_TYPE_DISABLED = 110
+
         //Для учеличения пула не используемых вьюХолдеров,чтобы не было ситуаций когда пул заполняется
         //только вьюхолдерами только одного и типа, и при необходимости переиспользовать вьюхолдер
         //не находит нужный тип и создает новый
---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListDiffCallback.kt
@@ -0,0 +1,32 @@
+package com.example.shoppinglist.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.shoppinglist.domain.ShopItem
+
+class ShopListDiffCallback(
+    private val oldList: List<ShopItem>,
+    private val newList: List<ShopItem>,
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    //проверяет являются ли обьекты одинаковыми
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return (oldList[oldItemPosition].id == newList[newItemPosition].id)
+    }
+
+    //проверяет содержимое обьектов
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        //у дата классов метод Equals переопределен поэтому можно так сделать
+        return oldItem == newItem
+    }
+
+}
