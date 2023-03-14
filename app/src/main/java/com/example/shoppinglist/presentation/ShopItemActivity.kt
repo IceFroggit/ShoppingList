dc3b97e6db85bbb0bc59e348b202bbd362a57a0d
IceFroggit <zeksengali0@gmail.com>
2023-03-14 16:19:51 +0300

    added ShopItemActivity and ShopItemViewModel with layout

---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,19 @@
+package com.example.shoppinglist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.shoppinglist.R
+
+class ShopItemActivity: AppCompatActivity() {
+
+    private lateinit var viewModel: ShopItemViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+        viewModel = ViewModelProvider(this).get(ShopItemViewModel::class.java)
+
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
@@ -0,0 +1,66 @@
+package com.example.shoppinglist.presentation
+
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.ShopListRepositoryImpl
+import com.example.shoppinglist.domain.AddShopItemUseCase
+import com.example.shoppinglist.domain.EditShopItemUseCase
+import com.example.shoppinglist.domain.GetShopItemUseCase
+import com.example.shoppinglist.domain.ShopItem
+
+class ShopItemViewModel : ViewModel() {
+
+    //todo переделать реализацию репозитория
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+
+    fun getShopItem(shopItemId: Int): ShopItem {
+        return getShopItemUseCase.getShopItem(shopItemId)
+    }
+
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        if (validateInput(name, count)) {
+            val shopItem = ShopItem(name, count, true)
+            addShopItemUseCase.addShopItem(shopItem)
+        }
+    }
+
+    fun editShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        if (validateInput(name, count)) {
+            val shopItem = ShopItem(name, count, true)
+            editShopItemUseCase.editShopItem(shopItem)
+        }
+    }
+
+    private fun parseName(inputName: String?): String {
+        return inputName?.trim() ?: ""
+    }
+
+    private fun parseCount(inputCount: String?): Int {
+        return try {
+            inputCount?.trim()?.toInt() ?: 0
+        } catch (e: Exception) {
+            0
+        }
+    }
+
+    private fun validateInput(name: String, count: Int): Boolean {
+        var result = true
+        if (name.isBlank()) {
+            //TODO: show error input name
+            result = false
+        }
+        if (count <= 0) {
+            //TODO: show error input count
+            result = false
+        }
+        return result
+    }
+}
